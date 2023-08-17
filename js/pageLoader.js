bb86a6416b27e53a994d2e4b5ee939b3530dda05
deboranuness <debora.nuness@hotmail.com>
2023-08-17 02:21:18 -0300

    Troca de páginas dinâmica
    Agora, ao clicar nas opções do menu, o conteúdo da página é atualizado sem recarregar a janela em que o site está aberto.
    
    Além disso, agora, o código do menu e rodapé estão inseridos apenas no arquivo index, e são mantidos visualmente mesmo com as trocas de página.
    
    Desta forma, para realizar alterações no header e/ou footer, é necessário alterar apenas um código (no index) e não em todas as páginas, como era feito anteriormente.

---
+++ js/pageLoader.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    loadContent("home.html");
+    const links = document.querySelectorAll("a[data-page]");
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const pageToLoad = this.getAttribute("data-page");
+            loadContent(pageToLoad);
+        });
+    });
+});
+
+function loadContent(page) {
+    fetch(page)
+        .then(response => response.text())
+        .then(content => {
+            const contentContainer = document.getElementById("content");
+            contentContainer.innerHTML = content;
+        })
+        .catch(error => console.error("Erro ao carregar conteúdo:", error));
+}
