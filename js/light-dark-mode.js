47754394e87b8b92edd71718f6efece0c7872eff
deboranuness <debora.nuness@hotmail.com>
2023-11-20 22:17:30 -0300

    Pop up theme mode
    Foi adicionado um pop up que aparece ao clicar no ícone de theme mode.
    
    Além disso, o tema muda para dark mode automaticamente, após o decorrer de 2s.

--- js/light-dark-mode.js
+++ js/light-dark-mode.js
@@ -1,10 +1,23 @@
 function toggleTheme() {
+    var body =document.body;
     var icon = document.getElementById("theme-button");
+    var popup = document.getElementById("emoji-popup");
 
-    document.body.classList.toggle("light-theme");
-    if(document.body.classList.contains("light-theme")){
-        icon.innerHTML = '<i class="fas fa-sun theme-icon"></i>';
-    } else {
+    body.classList.add("light-theme");
+    icon.innerHTML = '<i class="fas fa-sun theme-icon"></i>';
+    showPopup(popup);
+
+    setTimeout(function() {
+        hidePopup(popup);
         icon.innerHTML = '<i class="fas fa-moon theme-icon"></i>';
-    }
+        body.classList.remove("light-theme");
+    }, 1500);
+}
+
+function showPopup(popup) {
+    popup.style.display = "block";
+}
+
+function hidePopup(popup) {
+    popup.style.display = "none";
 }
